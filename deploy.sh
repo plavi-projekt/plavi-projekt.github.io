e5cce8593dcf85d9c55cbea0d946053291918dbd
PL4typus <pierre-louis.palant@insa-cvl.fr>
2019-05-08 18:39:52 +0200

    fixed syntax

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cp -R _site ..
+cp -r _site ..
 git checkout master
 FILES=$(ls)
 rm -rf $FILES
-cp -R ../_site/* .
+cp -r ../_site/* .
 git add .
 git commit -m $1
 git push -u origin master
